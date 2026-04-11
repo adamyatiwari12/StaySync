44318edfb0b104c864f1593383dd95bb9a26a3c2
adamyatiwari12 <adamyatiwari99@gmail.com>
2026-04-12 00:56:46 +0530

    fix(auth): prevent logout when clicking logo (navigation issue)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Banner from "@/components/home/Banner";
 import Banner2 from "@/components/home/Banner2";
 import Hero from "@/components/home/Hero";
@@ -10,23 +10,34 @@ import Footer from "@/components/layout/Footer";
 import LandingNavbar from "@/components/layout/LandingNavbar";
 import { testimonialData } from "@/components/data/testimonials";
 import Testimonial from "@/components/home/Testimonials";
+import Navbar from "@/components/layout/Navbar";
 
 const HomePage = () => {
+  const [role, setRole] = useState<"admin" | "tenant" | null>(null);
+
   useEffect(() => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
+    const token = localStorage.getItem("token");
+    if (!token) return;
+
+    const user = localStorage.getItem("user");
+    if (!user) return;
+
+    const parsedUser = JSON.parse(user);
+    if(parsedUser.role){
+      setRole(parsedUser.role);
+    }
   }, []);
 
   return (
     <div>
-      <LandingNavbar />
+      {role ? <Navbar role={role} /> : <LandingNavbar />}
       <Hero />
       <Services />
       <Banner />
       <Banner2 />
       <Testimonial testimonialData={testimonialData} />
       <Newsletter />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
